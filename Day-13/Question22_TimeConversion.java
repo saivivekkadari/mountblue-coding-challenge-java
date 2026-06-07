2ae4248a97915b4a916c341becd4793f87ddfd6d
Kadari Sai Vivek <142112603+saivivekkadari@users.noreply.github.com>
2026-06-07 21:28:30 +0530

    Create Question22_TimeConversion.java

---
+++ Day-13/Question22_TimeConversion.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+    // Write your code here
+    String hour=s.substring(0,2);
+    String minAndSec=s.substring(2,8);
+    String period=s.substring(8);
+    if(hour.equals("12")&&period.equals("AM")){
+        hour="00";
+    }
+    else if(!hour.equals("12")&&period.equals("PM")){
+        int a=Integer.parseInt(hour);
+        hour=String.valueOf(a+12);
+    }
+    return hour+minAndSec;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
