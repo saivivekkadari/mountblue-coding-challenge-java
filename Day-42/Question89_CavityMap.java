ccfcd59b634476f2a8bf716d99dce478ea6ce496
Kadari Sai Vivek <142112603+saivivekkadari@users.noreply.github.com>
2026-07-06 22:20:24 +0530

    Create Question89_CavityMap.java

---
+++ Day-42/Question89_CavityMap.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'cavityMap' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts STRING_ARRAY grid as parameter.
+     */
+
+    public static List<String> cavityMap(List<String> grid) {
+    // Write your code here
+    List<String>result=new ArrayList<>(grid);
+    
+    for(int i=1;i<grid.size()-1;i++){
+        char[] r=result.get(i).toCharArray();
+        for(int j=1;j<grid.size()-1;j++){
+            char c=grid.get(i).charAt(j);
+            if(c>grid.get(i-1).charAt(j)&&c>grid.get(i+1).charAt(j)&&c>grid.get(i).charAt(j-1)&&c>grid.get(i).charAt(j+1)){
+                r[j]='X';
+            }
+        }
+        result.set(i,new String(r));
+    }
+    return result;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> grid = IntStream.range(0, n).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .collect(toList());
+
+        List<String> result = Result.cavityMap(grid);
+
+        bufferedWriter.write(
+            result.stream()
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
