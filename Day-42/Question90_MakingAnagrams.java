46a1c19869e94999426a239dc991d352c4915fbb
Kadari Sai Vivek <142112603+saivivekkadari@users.noreply.github.com>
2026-07-06 22:32:09 +0530

    Create Question90_MakingAnagrams.java

---
+++ Day-42/Question90_MakingAnagrams.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'makingAnagrams' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static int makingAnagrams(String s1, String s2) {
+    // Write your code here
+    int count=0;
+    int arr[]=new int[26];
+    int arr1[]=new int[26];
+    for(int i=0;i<s1.length();i++){
+        arr[s1.charAt(i)-'a']++;
+    }
+    for(int j=0;j<s2.length();j++){
+        arr1[s2.charAt(j)-'a']++;
+    }
+    for(int k=0;k<arr.length;k++){
+        count=count+Math.abs(arr[k]-arr1[k]);
+    }
+    return count;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s1 = bufferedReader.readLine();
+
+        String s2 = bufferedReader.readLine();
+
+        int result = Result.makingAnagrams(s1, s2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
