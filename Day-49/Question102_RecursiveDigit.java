71aa761f9382fc48590d32adcb17aa2867c9ed3a
Kadari Sai Vivek <142112603+saivivekkadari@users.noreply.github.com>
2026-07-13 15:55:48 +0530

    Create Question102_RecursiveDigit.java

---
+++ Day-49/Question102_RecursiveDigit.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'superDigit' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING n
+     *  2. INTEGER k
+     */
+
+    public static int superDigit(String n, int k) {
+    // Write your code here
+    long sum=0;
+    
+    for(int i=0;i<n.length();i++){
+        sum=sum+Character.getNumericValue(n.charAt(i));
+    }
+    
+    sum=sum*k;
+    
+    while(sum>=10){
+        long d=0;
+    while(sum>0){
+        d=d+sum%10;
+        sum=sum/10;   
+    }
+    sum=d;
+    }
+    
+    return (int)sum;
+
+}}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        String n = firstMultipleInput[0];
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.superDigit(n, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
